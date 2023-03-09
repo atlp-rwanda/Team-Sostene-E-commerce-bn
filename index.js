38779f8d8df4e4df646c989a659391783f016d93
Pacific911 <nduwumwepacific@gmail.com>
2023-03-09 13:46:45 -0800

    chore(docker): setting the docker into the app

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
+// import mongoose from 'mongoose';
+const app = express();
+app.set('view engine', 'ejs');
 
-const app = express()
 
+app.use(express.json());
 dotenv.config()
 const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
     res.status(200).json('Hello World!')
-})
+});
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}!`))
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,3 +8,22 @@
  * a URL path/pattern, and a function that is called to handle that pattern
  * 
  */
+
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+app.set('view engine', 'ejs');
+// const { sequelize } = require('./config/db_connect');
+
+app.use(express.json());
+const dotenv = require('dotenv');
+dotenv.config();
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('Server has started!');
+});
+
+module.exports = app;
