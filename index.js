4ffb970e0583f259c628282de0e921f30f064d14
nsanzimfura eric <erickykress1@gmail.com>
2023-03-10 16:21:56 +0200

    check if the error is going to be solved

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import dotenv from 'dotenv'
 
 const app = express()
 
+ 
 dotenv.config()
 const PORT = process.env.PORT
+ 
 
 app.get('/', (req, res) => {
     res.status(200).json('Hello World!')
