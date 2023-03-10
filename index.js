097556b5b9aa4fcc7b39def00d852057dfe56df4
nsanzimfura eric <erickykress1@gmail.com>
2023-03-10 20:26:59 +0200

    dd

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ import dotenv from 'dotenv'
 
 const app = express()
 
-                    
-                    dotenv.config()
-                    const PORT = process.env.PORT
-                    
+dotenv.config()
+const PORT = process.env.PORT
 
-                    app.get('/', (req, res) => {
-                        res.status(200).json('Hello World!')
-                    })
+app.get('/', (req, res) => {
+    res.status(200).json('Hello World!')
+})
 
-                    app.listen(PORT, () => console.log(`app listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`app listening on port ${PORT}!`))
