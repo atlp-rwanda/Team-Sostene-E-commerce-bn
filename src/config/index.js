7decbc3ac88511f01e0a91d5b8092e5d704fa6d2
Pacific911 <nduwumwepacific@gmail.com>
2023-03-10 10:48:16 -0800

    chore(docker): setting up docker for the application
    - setting up docker for the application
    
    [Delivers #184651058]

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,15 @@
-// Here there will be config files
-
-/**
- *
- *  the configs folder, which keeps all the configs needed for the application.
- *  For example, if the app connects to a database, the configuration for the
- *  database (like database name and username) can be put in a file like db.config.js.
- *
- */
+const { Sequelize, DataTypes } = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  logging: false,
+});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+module.exports = { sequelize };
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,9 +2,9 @@
 
 // tip:
 /**
- * 
+ *
  * Controllers are responsible for handling
  * incoming requests and returning responses to the client.
  * A controller's purpose is to receive specific requests for the application.
- * 
+ *
  */
