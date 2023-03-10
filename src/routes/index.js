e672f40879d46067696d36ce21339ac4f21195ce
Ngabonziza Joseph <ntwarijoseph@gmail.com>
2023-03-10 11:04:22 +0200

    chore(coverage): integrate circleci with readme
    - ensure circleci is running all the tests
    - work with coveralls to provide the feedback on github
    
    [Delivers: #184651054]

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,5 +6,14 @@
  * A route is a section of Express code that
  * associates an HTTP verb ( GET , POST , PUT , DELETE , etc.),
  * a URL path/pattern, and a function that is called to handle that pattern
- *
+ * 
  */
+
+import Express from "express";
+const Router = Express.Router()
+
+Router.get('/test',(req,res)=>{
+    res.status(200).json({"message":"TESTED"})
+})
+
+export default Router
