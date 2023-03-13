afaaa59d3186bb5478dcdcbeb797e02c2db7ee95
Adrine UWERA <90565982+AdrineUWERA@users.noreply.github.com>
2023-03-13 10:12:53 +0200

    chore(coverage): integrate coveralls code coverage service (#4)
    - add coveralls to the github repository
    - setup code coverage tools in the project including mocha, chai, c8, and coveralls
    - set the minimum coverage to be 80 percent for lines, functions and branches
    
    [Delivers #184651053]

--- src/tests/index.js
+++ src/tests/index.js
@@ -4,5 +4,26 @@
 /**
  *
  * Here in tests folder is where all test files are.
- *
+ * 
  */
+
+import app from "../../index.js";
+import chai from "chai";
+import chaiHttp from "chai-http"
+
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('Testing the home route', () => { 
+    
+    it("should get the content of home route", function(done) {
+        chai
+        .request(app)
+        .get("/")
+        .end((err, response) => {
+          response.should.have.status(200); 
+          done();
+        });
+    })
+ })
