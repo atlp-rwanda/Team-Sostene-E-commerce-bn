3b2ab4236bb29392c35351f8071df71cdf48044b
nsanzimfura eric <erickykress1@gmail.com>
2023-03-14 18:09:58 +0200

    test

--- src/tests/index.js
+++ src/tests/index.js
@@ -1,12 +1,12 @@
-import app from '../../index.js';
 import chai from 'chai';
+import app from '../../index.js';
 import chaiHttp from 'chai-http';
 
 chai.should();
 
 chai.use(chaiHttp);
 
-describe('Testing the home route', () => {
+describe('Testing the home route', function () {
   it('should get the content of home route', function (done) {
     chai
       .request(app)
